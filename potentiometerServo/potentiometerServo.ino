fbf8b40abb6bfd6ed4f2b6fb4f592730fd5f7d83
Gavin Tranquilino <49102766+GuhBean@users.noreply.github.com>
2021-10-27 12:53:57 -0400

    Create Potentiometer Servo Project

---
+++ potentiometerServo/potentiometerServo.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+
+// Variables
+Servo servo;
+
+int potPin = 0; // Analog In
+int potVal;
+int mappedDegrees;
+int inputLag = 10;
+
+void setup() {
+  Serial.begin(9600);
+  servo.attach(9);
+  pinMode(potPin, INPUT);
+}
+
+void loop() {
+  potVal = analogRead(potPin); // reads potentiometer 0 - 1023
+  Serial.println(potVal);
+  mappedDegrees = map(potVal, 0, 1023, 0, 180); // scale it to servo 0 - 180
+  servo.write(mappedDegrees);
+  //Serial.println(mappedDegrees);
+  delay(inputLag);
+}
