d70b8ca8470096c04066be5591607200f620968d
Gavin Tranquilino <49102766+GuhBean@users.noreply.github.com>
2021-10-28 14:13:54 -0400

    Create nightLight.ino

---
+++ nightLight/nightLight.ino
@@ -0,0 +1,19 @@
+int photoPin = 0; //Analog
+int ledPin = 9;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(photoPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  int lightLvl = analogRead(photoPin);
+  Serial.println(lightLvl);
+  
+  // measured after testing range of photoresistor
+  lightLvl = map(lightLvl, 344, 1017, 0, 255);
+  
+  lightLvl = constrain(lightLvl, 0, 255);
+  analogWrite(ledPin, lightLvl);
+}
